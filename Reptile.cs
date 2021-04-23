125947101f30912472e2e8aeeced6bf538fa9668
♂ HappyMan <lovegf@hotmail.com>
2021-04-23 23:49:57 +0800

    添加项目文件。

---
+++ Reptile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace DoubleColoredBall
+{
+    public class Reptile
+    {
+        HttpHelper httpHelper = new HttpHelper();
+
+        public string GetHtml()
+        {
+            HttpItem httpItem = new HttpItem()
+            {
+                Host = "www.cwl.gov.cn",
+                URL = "http://www.cwl.gov.cn/cwl_admin/kjxx/findDrawNotice?name=ssq&issueCount=30",
+                Accept = "application/json, text/javascript, */*; q=0.01",
+                Referer = "http://www.cwl.gov.cn/kjxx/ssq/kjgg/",
+                ResultCookieType = ResultCookieType.CookieCollection,
+                ResultType = ResultType.String
+               
+                
+            };
+            
+            HttpResult httpResult = httpHelper.GetHtml(httpItem);
+            string jsonString = httpResult.Html;
+            
+            //解析json
+
+
+            return jsonString;
+        }
+    }
+}
